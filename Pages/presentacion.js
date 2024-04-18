4b26e1175011f280af5ae71073c1636c6ebc8ce2
Wagner Jafet Matos Pe�a <20220137@itla.edu.do>
2024-04-18 15:03:05 -0400

    Metodo buscar implementado en la vista Libros

--- Pages/presentacion.js
+++ Pages/presentacion.js
@@ -1,9 +1,41 @@
-// script.js
-
-// Realizar una solicitud HTTP GET a tu endpoint de libros
+function cargarLibros(){
+  // Realizar una solicitud HTTP GET a tu endpoint de libros
 fetch('https://localhost:7037/api/Libro')
-  .then(response => response.json())
+.then(response => response.json())
+.then(data => {
+  document.getElementById('card-father').innerHTML = '';
+
+  // Manipular los datos recibidos (por ejemplo, crear las tarjetas de libro)
+  data.data.forEach(libro => {
+    let card = `
+      <div class="card" style="width: calc(20% - 15px); margin-bottom: 20px; color: black !important;">
+        <img class="card-img-top" src="${libro.urlImagen}" alt="Card image cap">
+        <div class="card-body" style="color: black !important;">
+          <h5 class="card-title">${libro.titulo}</h5>
+          <a href="/Pages/plantillas/detalles.html?id=${libro.id}" class="btn btn-dark">Detalles</a>
+        </div>
+      </div>
+    `;
+    if(document.getElementById('card-father') != null){
+      document.getElementById('card-father').innerHTML += card;
+    }
+    
+  });
+})
+.catch(error => console.error('Error:', error));
+}
+
+
+  function buscar() {
+    var searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm != ""){
+      fetch(`https://localhost:7037/api/Libro/searchTerm?searchTerm=${searchTerm}`)
+    .then(response => response.json() )
+    
   .then(data => {
+    if(data.success == true){
+      document.getElementById('card-father').innerHTML = '';
+
     // Manipular los datos recibidos (por ejemplo, crear las tarjetas de libro)
     data.data.forEach(libro => {
       let card = `
@@ -20,23 +52,14 @@ fetch('https://localhost:7037/api/Libro')
       }
       
     });
+    }
+    
   })
   .catch(error => console.error('Error:', error));
+    }
+    else{
+      cargarLibros();
+    }
+}
 
-
-  // presentacion.js
-
-// // Función para manejar el clic en el botón "Detalles"
-// function mostrarDetalles(id) {
-//     // Redirigir a la página de detalles del libro con el ID específico
-//     window.location.href = `/Pages/plantillas/detalles.html?id=${id}`;
-//   }
-  
-//   // Agregar un event listener a todos los botones "Detalles" de los libros
-//   document.querySelectorAll('.btn-detalle').forEach(btn => {
-//     btn.addEventListener('click', () => {
-//       // Obtener el ID del libro desde el atributo data-id del botón
-//       let idLibro = btn.getAttribute('data-id');
-//       mostrarDetalles(idLibro);
-//     });
-//   });
+cargarLibros();
